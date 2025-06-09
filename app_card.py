fb961a98465f52263fcf049fa7e623b644bf0fed
Carlos Quiala <cquiala12@gmail.com>
2025-06-09 02:41:31 -0400

    feat: Add shared link to use with codespace

--- app_card.py
+++ app_card.py
@@ -641,9 +641,9 @@ if __name__ == "__main__":
         load_resources()
         app = create_cards_interface()
         app.launch(
-            server_name="0.0.0.0",
-            server_port=7860,
-            share=False,
+            #server_name="0.0.0.0",
+            #server_port=7860,
+            share=True,
             show_error=True
         )
     except Exception as e:
